8eeb2af3ebcb5ad6381773d37f34c01e4888c469
James Chapman <james.chapman.19@ucl.ac.uk>
2021-07-27 18:03:20 +0100

    quickperms start

--- skperm/permutation_tests/palm_cca.py
+++ skperm/permutation_tests/palm_cca.py
@@ -1,2 +1,51 @@
-def cca_perm():
-    pass
+import numpy as np
+from typing import Tuple
+from .permutation_test import _BasePermutationTest
+
+
+class CCAPermutationTest(_BasePermutationTest):
+    def __init__(self, model, scoring=None, cv=None, permuter=None, permutations=100, random_state=0, verbose=0,
+                 n_jobs=None, fit_params=None):
+        super().__init__(model, scoring, cv, permuter, permutations, random_state, verbose, n_jobs, fit_params)
+
+    def _process(self, wilks=True, lawley_hotelling=True, pillai=True, roy_ii=True, roy_iii=True):
+        metrics = {}
+        if wilks:
+            metrics['wilks'] = self._wilks()
+        if lawley_hotelling:
+            metrics['lawley_hotelling'] = self._lawley_hotelling()
+        if pillai:
+            metrics['pillai'] = self._pillai()
+        if roy_ii:
+            metrics['roy_ii'] = self._roy_ii()
+        if roy_iii:
+            metrics['roy_iii'] = self._roy_iii()
+
+    def _wilks(self, E, H):
+        return np.linalg.det(E) / np.linalg.det(E + H)
+
+    def _lawley_hotelling(self):
+        pass
+
+    def _pillai(self):
+        pass
+
+    def _roy_ii(self):
+        pass
+
+    def _roy_iii(self):
+        pass
+
+
+def _get_correlations(model, X: Tuple[np.ndarray]) -> np.ndarray:
+    Z = model.transform(X)
+    correlations = np.corr
+    return correlations
+
+
+def cca_perm(model, X: Tuple[np.ndarray], get_correlations=None):
+    if get_correlations is None:
+        get_correlations = _get_correlations
+    model.fit(X)
+    correlations = get_correlations()
+    wilks_lambda = -np.fliplr(np.cumsum(np.log(1 - correlations ** 2)))
---
+++ skperm/utils/validation/validation.py
@@ -0,0 +1,78 @@
+import numbers
+from collections.abc import Iterable
+
+from sklearn.model_selection._split import StratifiedKFold, KFold, _CVIterableWrapper
+from sklearn.utils.multiclass import type_of_target
+
+
+def check_permuter(permuter,X):
+    """Input checker utility for building a cross-validator
+    Parameters
+    ----------
+
+    Returns
+    -------
+    checked_cv : a cross-validator instance.
+        The return value is a cross-validator which generates the train/test
+        splits via the ``split`` method.
+    """
+    return permuter
+
+
+# Author: Alexandre Gramfort <alexandre.gramfort@inria.fr>
+#         Gael Varoquaux <gael.varoquaux@normalesup.org>
+#         Olivier Grisel <olivier.grisel@ensta.org>
+#         Raghav RV <rvraghav93@gmail.com>
+#         Leandro Hermida <hermidal@cs.umd.edu>
+#         Rodion Martynov <marrodion@gmail.com>
+# License: BSD 3 clause
+def check_cv(cv=5, y=None, *, classifier=False):
+    """Input checker utility for building a cross-validator
+    Parameters
+    ----------
+    cv : int, cross-validation generator or an iterable, default=None
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+        - None, to use the default 5-fold cross validation,
+        - integer, to specify the number of folds.
+        - :term:`CV splitter`,
+        - An iterable yielding (train, test) splits as arrays of indices.
+        For integer/None inputs, if classifier is True and ``y`` is either
+        binary or multiclass, :class:`StratifiedKFold` is used. In all other
+        cases, :class:`KFold` is used.
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validation strategies that can be used here.
+        .. versionchanged:: 0.22
+            ``cv`` default value changed from 3-fold to 5-fold.
+    y : array-like, default=None
+        The target variable for supervised learning problems.
+    classifier : bool, default=False
+        Whether the task is a classification task, in which case
+        stratified KFold will be used.
+    Returns
+    -------
+    checked_cv : a cross-validator instance.
+        The return value is a cross-validator which generates the train/test
+        splits via the ``split`` method.
+    """
+    cv = 5 if cv is None else cv
+    if isinstance(cv, numbers.Integral):
+        if (
+            classifier
+            and (y is not None)
+            and (type_of_target(y) in ("binary", "multiclass"))
+        ):
+            return StratifiedKFold(cv)
+        else:
+            return KFold(cv)
+
+    if not hasattr(cv, "split") or isinstance(cv, str):
+        if not isinstance(cv, Iterable) or isinstance(cv, str):
+            raise ValueError(
+                "Expected cv as an integer, cross-validation "
+                "object (from sklearn.model_selection) "
+                "or an iterable. Got %s." % cv
+            )
+        return _CVIterableWrapper(cv)
+
+    return cv  # New style cv objects are passed without any modification
